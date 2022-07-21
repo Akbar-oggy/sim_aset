f5ebdea0c8c41f236d76fc6d195d7a9d46515560
Akbar-oggy <akbarabdansa@gmail.com>
2022-07-21 08:06:59 +0700

    mengubah footer JSFunctionTable

--- app/footer.php
+++ app/footer.php
@@ -55,13 +55,13 @@
     <script src="plugins/datatables-buttons/js/buttons.print.min.js"></script>
     <script src="plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
     <!-- Page specific script -->
-    <!-- <script>
+    <script>
         $(function() {
             $("#example1").DataTable({
                 "responsive": true,
                 "lengthChange": false,
                 "autoWidth": false,
-                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+                "buttons": ["colvis"]
             }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
             $('#example2').DataTable({
                 "paging": true,
@@ -73,4 +73,4 @@
                 "responsive": true,
             });
         });
-    </script> -->
+    </script>
